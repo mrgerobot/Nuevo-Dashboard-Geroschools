d3c1bc5247e87e57325ae37b1a673eeed7aa8ed1
juanivila <juan.vilabach@unifr.ch>
2026-01-28 15:37:53 -0300

    buscador de perfil del estudiante

--- src/pages/PerfilEstudiante.tsx
+++ src/pages/PerfilEstudiante.tsx
@@ -465,7 +465,7 @@ export default function PerfilEstudiante() {
             </TabsContent>
 
             <TabsContent value="carreras" className="space-y-6">
-              {vocational ? (
+              {vocational.avanceAutoconocimiento == "Completo" ? (
                 <>
                   {/* Por intereses */}
                   <div className="bg-card rounded-xl border border-border p-6">
