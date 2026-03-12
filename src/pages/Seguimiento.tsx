982e0536783ba9781787a20b88359771cba645e5
juanivila <juan.vilabach@unifr.ch>
2026-03-12 18:27:09 -0300

    idk

--- src/pages/Seguimiento.tsx
+++ src/pages/Seguimiento.tsx
@@ -248,7 +248,7 @@ export default function Seguimiento() {
                     <StatusChip status={record.interaccionCoach} />
                   </td> */}
                   <td>
-                    {record.avanceAutoconocimiento == "Completo" && record.carrerasRecomendadasPorFortalezas[0] != "" ? (
+                    {record.avanceAutoconocimiento == "Completo" && record.carrerasRecomendadasPorFortalezas?.length > 0 && record.carrerasRecomendadasPorFortalezas[0] != "" ? (
                       <Button
                       variant="ghost"
                       size="sm"
